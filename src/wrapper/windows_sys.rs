55dad09fefa180d231a66fcec863f92bf33039bf
Robert Bragg <robert@sixbynine.org>
2025-02-10 23:47:17 +0000

    Add MultiByteToWideChar binding

--- src/wrapper/windows_sys.rs
+++ src/wrapper/windows_sys.rs
@@ -8,12 +8,15 @@
     clippy::all
 )]
 windows_targets::link!("kernel32.dll" "system" fn GetACP() -> u32);
+windows_targets::link!("kernel32.dll" "system" fn MultiByteToWideChar(codepage : u32, dwflags : MULTI_BYTE_TO_WIDE_CHAR_FLAGS, lpmultibytestr : PCSTR, cbmultibyte : i32, lpwidecharstr : PWSTR, cchwidechar : i32) -> i32);
 windows_targets::link!("kernel32.dll" "system" fn WideCharToMultiByte(codepage : u32, dwflags : u32, lpwidecharstr : PCWSTR, cchwidechar : i32, lpmultibytestr : PSTR, cbmultibyte : i32, lpdefaultchar : PCSTR, lpuseddefaultchar : *mut BOOL) -> i32);
 pub type BOOL = i32;
 pub const CP_UTF7: u32 = 65000u32;
 pub const CP_UTF8: u32 = 65001u32;
+pub type MULTI_BYTE_TO_WIDE_CHAR_FLAGS = u32;
 pub type PCSTR = *const u8;
 pub type PCWSTR = *const u16;
 pub type PSTR = *mut u8;
+pub type PWSTR = *mut u16;
 pub const WC_COMPOSITECHECK: u32 = 512u32;
 pub const WC_NO_BEST_FIT_CHARS: u32 = 1024u32;
